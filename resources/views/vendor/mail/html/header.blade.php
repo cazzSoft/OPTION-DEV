1053c67b0c4bad7890eab6564bbd85fd8872869f
Cristhian Zambrano <cazzdj17@hotmail.com>
2022-10-12 12:37:58 -0500

    imagen error con hotmail

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,14 +3,13 @@
 <br>
 <a href="{{ $url }}" style="display: inline-block;">
 	
-		@if (trim($slot) === 'Laravel')
-		{{-- <img src="{{asset('img/logo2.png')}}" class="logo" alt="Laravel Logo"> --}}
-		{{-- <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo"> --}}
-		@else
-		{{-- <img src="https://option2health.com/img/logo2.png" class="logo" alt="Laravel Logo"> <br>
-		<img src="https://option2health.com/img/personas.png" class="icon-person" alt="Laravel Logo"> --}}
-		{{-- {{ $slot }} --}}
-		@endif
+@if (trim($slot) === 'Laravel')
+<span> no deberia salir xs</span>
+{{-- <img src="{{asset('img/logo2.png')}}" class="logo" alt="Laravel Logo"> --}}
+{{-- <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo"> --}}
+@else
+
+@endif
 	
 </a>
 </td>
--- resources/views/vendor/mail/html/header_2.blade.php
+++ resources/views/vendor/mail/html/header_2.blade.php
@@ -2,15 +2,16 @@
 <td class="header">
 <br>
 <a href="{{ $url }}" style="display: inline-block;">
-	
-		@if (trim($slot) === 'Laravel')
-		<img src="{{asset('img/logo2.png')}}" class="logo" alt="Laravel Logo">
-		{{-- <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo"> --}}
-		@else
-		<img src="{{asset('img/logo2.png')}}" class="logo" alt="Laravel Logo"> <br>
-		<img src="{{asset('img/personas.png')}}" class="icon-person" alt="personas-Logo-img">
-		{{-- {{ $slot }} --}}
-		@endif
+
+@if (trim($slot) === 'Laravel')
+<span> esto img arriba 2</span>
+{{-- <img src="{{asset('img/logo2.png')}}" class="logo" alt="Laravel Logo"> --}}
+{{-- <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo"> --}}
+@else
+<img src="{{asset('img/logo2.png')}}" class="logo" alt="Laravel Logo"> <br>
+<img src="{{asset('img/personas.png')}}" class="icon-person" alt="personas-Logo-img">
+{{-- {{ $slot }} --}}
+@endif
 	
 </a>
 </td>
