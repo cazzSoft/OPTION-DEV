e44514a772365537e313abd8f97d2c000274e61a
Cristhian Zambrano <cazzdj17@hotmail.com>
2022-10-12 14:05:50 -0500

    test email hotmail

--- resources/views/mail/test.blade.php
+++ resources/views/mail/test.blade.php
@@ -1,4 +1,4 @@
-@component('mail::message')
+{{-- @component('mail::message')
 <h1 class="titulo1">Notificacion prueba</h1>
 <img src="{{asset('img/personas.png')}}" class="icon-person test" alt="personas-Logo-img">
 	
@@ -7,5 +7,18 @@
 @endcomponent
  
 Gracias,<br>
+{{ config('app.name') }}
+@endcomponent --}}
+
+@component('mail::message')
+# Order Shipped
+ 
+Your order has been shipped!
+ 
+@component('mail::button', ['url' => $url])
+View Order
+@endcomponent
+ 
+Thanks,<br>
 {{ config('app.name') }}
 @endcomponent
