d7016a50a6cc3929ca1601f75b3a9da1313b1909
Cristhian Zambrano <cazzdj17@hotmail.com>
2022-10-12 20:40:15 -0500

    hiolas

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -4,11 +4,11 @@
 <a href="{{ $url }}" style="display: inline-block;">
 	
 @if (trim($slot) === 'Laravel')
-<span> no deberia salir xs</span>
+
 {{-- <img src="{{asset('img/logo2.png')}}" class="logo" alt="Laravel Logo"> --}}
 {{-- <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo"> --}}
 @else
-<span> no deberia salir xs</span>
+
 @endif
 	
 </a>
--- resources/views/vendor/mail/html/header_2.blade.php
+++ resources/views/vendor/mail/html/header_2.blade.php
@@ -9,7 +9,7 @@
 {{-- <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo"> --}}
 @else
 <img src="{{asset('img/logo2.png')}}" class="logo" alt="Laravel Logo"> 
-{{-- <img src="{{asset('img/personas.png')}}" class="icon-person" alt="personas-Logo-img"> --}}
+<img src="{{asset('img/personas.png')}}" class="icon-person" alt="personas-Logo-img">
 {{-- {{ $slot }} --}}
 @endif
 	
